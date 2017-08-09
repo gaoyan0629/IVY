643f2f073909f7d36ac8c836c5c51253f5bac029
gaoyan0629 <gaoyan0629@gmail.com>
2017-08-09 15:25:05 +0800

    initial setup

--- bip.py
+++ bip.py
@@ -1,3 +1,8 @@
-import os
-import sys
-this is added for feature_x
+class Student(object):
+    def __init__(self, id, name, marks):
+        self.id = id
+        self.name = name
+        self.marks = marks
+
+    def __str__(self):
+        return '%s has marks %s' % (self.name, self.marks)
