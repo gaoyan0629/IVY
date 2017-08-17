a8013e97bfefaa0997145868907d7dedd0768f11
Yan GAO <gaoyangao@dbs.com>
2017-08-17 15:39:55 +0800

    delete file

--- bip.py
+++
@@ -1,8 +0,0 @@
-class Student(object):
-    def __init__(self, id, name, marks):
-        self.id = id
-        self.name = name
-        self.marks = marks
-
-    def __str__(self):
-        return '%s has marks %s' % (self.name, self.marks)
--- test.py
+++
@@ -1,3 +0,0 @@
-import sys
-import os
-from subprocess import call
